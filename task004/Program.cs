de3c62d39cea7cf6bc5ff2480badadd284fddad4
Alexander Kamaev <qalex2@yandex.ru>
2023-07-06 12:13:16 +0300

    Решена задача 004

---
+++ task004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int number3 = int.Parse(Console.ReadLine());
+int max = 0;
+
+if (number1 == number2 & number2 == number3) Console.WriteLine("Все три числа равны между собой!");
+else
+{
+    if (number1 > number2 & number1 > number3) max = number1;
+    if (number2 > number1 & number2 > number3) max = number2;
+    if (number3 > number1 & number3 > number2) max = number3;
+    Console.WriteLine($"Максимум среди чисел {number1}, {number2} и  {number3} равен числу " + max + " !");
+}    
